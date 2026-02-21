0b2fa17e21f82bc777264bab583a23d951e4158f
Steve Hudson <sh1448291904@gmail.com>
2026-02-22 03:37:07 +1000

    spurious apostrophe

--- lib/weird.rb
+++ lib/weird.rb
@@ -4,9 +4,8 @@ require 'weird/logging'
 
 # main Weird code
 module Weird
- class Prepositions
-    Prepositions = %w[about above across after against along amid among around at before behind below beneath beside besides between beyond by concerning considering despite down during except for from in inside into like near of off on onto opposite out outside over past per regarding round since than through throughout till to toward towards under underneath unlike until up upon via with within without].freeze` 
+  class Prepositions
+    Prepositions = %w[about above across after against along amid among around at before behind below beneath beside besides between beyond by
+                      concerning considering despite down during except for from in inside into like near of off on onto opposite out outside over past per regarding round since than through throughout till to toward towards under underneath unlike until up upon via with within without].freeze
   end
-
-
 end
